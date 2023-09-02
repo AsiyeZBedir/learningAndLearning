458a96de2f8e2d5af79886dd6928f2087629f3f4
Asiye <116940286+liaail@users.noreply.github.com>
2023-09-02 11:14:03 +0300

    Update npmgpt.js

--- npmgpt.js
+++ npmgpt.js
@@ -1,10 +1,16 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Ana sayfa
 app.get('/', (req, res) => {
   res.send('Merhaba, Dünya!');
+});
+
+// Hakkında sayfası
+app.get('/about', (req, res) => {
+  res.send('Hakkında Sayfası');
 });
 
 app.listen(port, () => {
