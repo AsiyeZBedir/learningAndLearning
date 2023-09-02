9badaeab832018ed47fd3d83ba9ac87566953205
Asiye <116940286+liaail@users.noreply.github.com>
2023-09-02 11:32:41 +0300

    Create toDoListLL.js

---
+++ toDoListLL.js
@@ -0,0 +1,29 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('To-Do List App');
+});
+
+app.listen(port, () => {
+  console.log(`Uygulama http://localhost:${port} adresinde çalışıyor`);
+});
+
+// app.js (Devamı)
+const todos = [];
+
+// Yapılacakları listeleme
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+// Yapılacak ekleme
+app.post('/todos', (req, res) => {
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.json(newTodo);
+});
