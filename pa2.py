e9d6436c4124847aa5c983ac472ea8acae8dfb03
Asiye <116940286+liaail@users.noreply.github.com>
2023-09-09 23:00:40 +0300

    Create pa2.py

---
+++ pa2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# CSV dosyasından veri okuma
+data = pd.read_csv('veri.csv')
+
+# Veriyi inceleme
+print("Veri Özeti:")
+print(data.head())
+
+# Temel istatistiksel bilgileri alma
+summary_stats = data.describe()
+print("\nİstatistiksel Bilgiler:")
+print(summary_stats)
+
+# Veri görselleştirme
+data.plot(kind='bar', x='Ad', y='Yaş', title='Yaş Grafiği')
+plt.show()
